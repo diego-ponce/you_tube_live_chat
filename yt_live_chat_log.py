75f6d5051f4b5ad970b28f2cbb0982769a8bebb8
diego-ponce <ponce.diego.e@gmail.com>
2020-05-17 21:58:59 -0700

    initial commit

---
+++ yt_live_chat_log.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# setup driver
+chrome_options = Options()
+#chrome_options.add_argument("--disable-extensions")
+#chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(options=chrome_options)
+
+
+#TODO dynamically grab this from channel using YouTube API
+base_url = "https://www.youtube.com/live_chat?v="
+chat_id = "bfGaUembwA8"
+url = base_url + str(chat_id)
+
+# TODO move to headless chromium or firefox
+# driver = webdriver.Chrome()
+driver.get(url)
+messages = driver.find_element_by_id('chat-messages')
+item_list = messages.find_element_by_id('item-list')
+item = item_list.find_element_by_id('items')
+messages = item.find_elements_by_tag_name('yt-live-chat-text-message-renderer')
+
+for message in messages:
+    author = message.find_element_by_id('author-name').text
+    message = message.find_element_by_id('message').text
+    print("{}: {}".format(author, message))
+
+# close out
+driver.quit()
