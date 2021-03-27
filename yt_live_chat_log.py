a077abce3cf906bfeaca777c006a4c1efe25560e
diego-ponce <ponce.diego.e@gmail.com>
2021-03-27 16:54:16 -0700

    removed unused packages from requirements, fixed bug in headless mode

--- yt_live_chat_log.py
+++ yt_live_chat_log.py
@@ -1,11 +1,18 @@
+'''Using Selenium in headless mode to access youtube comments'''
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
+# import sys
+# import os
+# import httplib2
+
+
 # setup driver
 chrome_options = Options()
-#chrome_options.add_argument("--disable-extensions")
-#chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--headless")
+# headless mode needs a user-agent tag otherwise yt thinks its an old browser
+chrome_options.add_argument("user-agent=User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")
 driver = webdriver.Chrome(options=chrome_options)
 
 
@@ -14,8 +21,7 @@ base_url = "https://www.youtube.com/live_chat?v="
 chat_id = "bfGaUembwA8"
 url = base_url + str(chat_id)
 
-# TODO move to headless chromium or firefox
-# driver = webdriver.Chrome()
+# get the page and process the messages
 driver.get(url)
 messages = driver.find_element_by_id('chat-messages')
 item_list = messages.find_element_by_id('item-list')
